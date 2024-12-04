e8ac1d389f9488bb2ff60d280071bdf014ba4409
Dmitriy Khalko <dxalko@gmail.com>
2024-12-04 19:23:27 +0300

    hw08

--- hw08_binary_search/main.go
+++ hw08_binary_search/main.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+func binarySearch(storage []int, s int) int {
+	minIndex := 0
+	maxIndex := len(storage) - 1
+	for minIndex <= maxIndex {
+		midIndex := (minIndex + maxIndex) / 2
+
+		if s == storage[midIndex] {
+			return s
+		} else if s < storage[midIndex] {
+			maxIndex = midIndex - 1
+		} else {
+			minIndex = midIndex + 1
+		}
+	}
+	return -1
+}
+
 func main() {
-	// Place your code here.
+	st := []int{11, 22, 33, 42, 54, 65, 78, 92, 99}
+	fmt.Println(binarySearch(st, 65))
 }
