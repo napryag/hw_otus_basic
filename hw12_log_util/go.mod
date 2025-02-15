fea107c6d845252fabaa743b2d511b534469291d
Dmitriy Khalko <dxalko@gmail.com>
2025-02-15 19:55:18 +0300

    hw12 completed

--- hw12_log_util/go.mod
+++ hw12_log_util/go.mod
@@ -1,3 +1,10 @@
-module github.com/fixme_my_friend/hw12_log_util
+module github.com/napryag/hw_otus_basic/hw12_log_util
 
 go 1.20
+
+require github.com/sirupsen/logrus v1.9.3
+
+require (
+	github.com/spf13/pflag v1.0.6
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- hw12_log_util/main.go
+++ hw12_log_util/main.go
@@ -1,5 +1,95 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+)
+
+type Config struct {
+	fileFlag   string
+	levelFlag  string
+	outputFlag string
+}
+
 func main() {
-	// Place your code here.
+	var config Config
+	if err := os.Setenv("LOG_ANALYZER_FILE", "asd.log"); err != nil {
+		logrus.Fatalf("set env LOG_ANALYZER_FILE error: %s", err)
+	}
+	if err := os.Setenv("LOG_ANALYZER_LEVEL", "error"); err != nil {
+		logrus.Fatalf("set env LOG_ANALYZER_LEVEL error: %s", err)
+	}
+	if err := os.Setenv("LOG_ANALYZER_OUTPUT", "b.txt"); err != nil {
+		logrus.Fatalf("set env LOG_ANALYZER_OUTPUT error: %s", err)
+	}
+
+	pflag.StringVarP(&config.fileFlag, "file", "f", "", "log file path")
+	pflag.StringVarP(&config.levelFlag, "level", "l", "info", "chooses level to analyze")
+	pflag.StringVarP(&config.outputFlag, "output", "o", "", "path to log file write")
+
+	pflag.Lookup("file").NoOptDefVal = os.Getenv("LOG_ANALYZER_FILE")
+	pflag.Lookup("level").NoOptDefVal = os.Getenv("LOG_ANALYZER_LEVEL")
+	pflag.Lookup("output").NoOptDefVal = os.Getenv("LOG_ANALYZER_OUTPUT")
+
+	pflag.Parse()
+
+	stats, err := analyzeLogFile(config.fileFlag, config.levelFlag)
+	if err != nil {
+		logrus.Fatalf("cant analyze file: %s", err)
+	}
+
+	err = outputStats(stats, config.outputFlag)
+	if err != nil {
+		logrus.Fatalf("output error: %s", err)
+	}
+}
+
+func analyzeLogFile(filePath, logLevel string) (map[string]int, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("open file: %w", err)
+	}
+	defer file.Close()
+
+	stats := make(map[string]int)
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, logLevel) {
+			stats[logLevel]++
+		}
+	}
+
+	if err = scanner.Err(); err != nil {
+		return nil, fmt.Errorf("scan error: %w", err)
+	}
+
+	return stats, nil
+}
+
+func outputStats(stats map[string]int, outputFile string) error {
+	var output *os.File
+	var err error
+
+	if outputFile != "" {
+		output, err = os.Create(outputFile)
+		if err != nil {
+			return fmt.Errorf("create file error: %w", err)
+		}
+		defer output.Close()
+	} else {
+		output = os.Stdout
+	}
+
+	for level, count := range stats {
+		fmt.Fprintf(output, "Log level: %s, Number of entries: %d\n", level, count)
+	}
+
+	return nil
 }
